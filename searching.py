f4ade451fb3509382c2609b06d8ec6f54679e40d
Slavíčková Tereza (271405) <271405@vutbr.cz>
2025-04-07 13:07:45 +0200

    ulozeni

--- searching.py
+++ searching.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 # get current working directory path
 cwd_path = os.getcwd()
@@ -12,6 +13,15 @@ def read_data(file_name, field):
     :return: (list, string),
     """
     file_path = os.path.join(cwd_path, file_name)
+    with open (file_path, "r") as file_obj:
+        data = json.load(file_obj)
+    if field in data.keys():
+        return data[field]
+    else:
+        print(f"Field {field} not exist!")
+        return None
+
+
 
 
 def main():
@@ -20,3 +30,5 @@ def main():
 
 if __name__ == '__main__':
     main()
+    sequential_data = read_data("sequential.json", "unordered_numbers")
+    print(sequential_data)
